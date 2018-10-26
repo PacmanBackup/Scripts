6701d6b29d738977336f7167f05dee3cbd6b6563
John Bennett <themicrowavesnameistimothy@gmail.com>
2018-10-25 20:05:43 -0600

    dmesg now works correctly in the script.

--- SpectreMeltdown.sh
+++ SpectreMeltdown.sh
@@ -3,7 +3,8 @@
 
 #!/bin/bash
 #echo "Checking cpu architecture and status..." (can be useful on certain hardware)
-#cat /proc/cpuinfo
+echo "Get CPU info..."
+sudo cat /proc/cpuinfo
 
 echo "Checking whether cpu is effected by meltdown/spectre"
 grep cpu_insecure /proc/cpuinfo
@@ -13,10 +14,10 @@ grep CONFIG_PAGE_TABLE_ISOLATION=y /boot/config-`uname -r` &&
 echo "patched :)" || echo "unpatched :("
 
 echo "If kernel config file does not exist in /boot check status of kpti via dmesg..."
-dmesg | grep "Kernel/User page tables isolation: enabled" && echo "patched" || echo "unpatched" 
+sudo dmesg | grep "Kernel/User page tables isolation: enabled" && echo "patched" || echo "unpatched" 
  
 
 # Alternate method if config file does not exist (enable if needed)
-# grep -sq "x86/pti" /var/log/dmesg && echo "Patched"
+# grep  "x86/pti" /var/log/dmesg && echo "Patched"
 
 
